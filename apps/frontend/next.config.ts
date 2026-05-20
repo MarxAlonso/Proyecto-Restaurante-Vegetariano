554d4bc3dc75ac93d798528e18b02f64c4604635
MarxAlonso <marxchip99@gmail.com>
2026-05-19 22:05:11 -0500

    next config

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://accounts.google.com; connect-src 'self' https://restaurante-vegetariano-backend.vercel.app; frame-src https://accounts.google.com;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
